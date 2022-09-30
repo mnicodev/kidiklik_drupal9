b2ee850ec568da2b478c2482e2545b4a36d7e751
mnicodev <test@3wou.net>
2022-09-30 18:46:17 +0200

    js

--- web/themes/contrib/kidiklik/js/scripts.js
+++ web/themes/contrib/kidiklik/js/scripts.js
@@ -13,7 +13,7 @@ Drupal.behaviors.kidiklik = {
 		var charging_blocs = jQuery('[data-big-pipe-placeholder-id]').length;
 		console.log(charging_blocs)
 	      	if (charging_blocs ===1 || charging_blocs === 0){
-			jQuery('#block-carre1copie').after(jQuery('#block-carreblock').html());
+			jQuery('#block-carre1copie').html(jQuery('#block-carreblock').html());
 	jQuery('select[name="ville"]').select2({
 		placeholder: 'Choisissez une ville ...',
 		allowClear: true,
