2f42d1f7069a439043557bac339b9b121e648f4c
mnicodev <test@3wou.net>
2023-08-09 12:24:57 +0200

    bouton prochaine event

--- web/modules/custom/kidiklik_front/src/Plugin/Block/SortiesBoutonBlock.php
+++ web/modules/custom/kidiklik_front/src/Plugin/Block/SortiesBoutonBlock.php
@@ -2,7 +2,11 @@
 
 namespace Drupal\kidiklik_front\Plugin\Block;
 
+
 use Drupal\Core\Block\BlockBase;
+use Drupal\views\Views;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Provides a 'SortiesBoutonBlock' block.
@@ -31,18 +35,34 @@ class SortiesBoutonBlock extends BlockBase
     $build['#theme'] = 'sortie_moment_bouton';
     $node = \Drupal::request()->get('node');
 
+    $seach_event = Views::getView("activites");
+    $seach_event->setDisplay("search_event");
+    
+    
+
     if (!empty($node)) {
       if ($node->getType() === 'activite') {
         $build['#ref_act'] = $node->id();
+        $ref_adherent = $node->id();
       } else {
         $build['#ref_adh'] = current($node->get('field_adherent')->getValue())['target_id'];
+        $ref_adherent = $build['#ref_adh'];
       }
     }
+    
+    $seach_event->setArguments([$ref_adherent]);
+		$seach_event->execute();
+    $events = \Drupal::service('renderer')->render($seach_event->render());
+    $count_event = json_decode($events->__toString());
+    
     if (empty($build['#ref_act']) || empty($build['#ref_adh'])) {
      // return null;
     }
-
-    return $build;
+    if(count($count_event) > 0) {
+      return $build;
+    }
+    
+    
   }
 
 }
