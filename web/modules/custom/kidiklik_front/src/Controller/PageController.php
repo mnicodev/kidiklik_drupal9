c7fc592014bb8e3b37f4512e06efac2114d32bfe
mnicodev <test@3wou.net>
2024-03-25 20:53:53 +0100

    contact page

--- web/modules/custom/kidiklik_front/src/Controller/PageController.php
+++ web/modules/custom/kidiklik_front/src/Controller/PageController.php
@@ -83,6 +83,28 @@ class PageController extends ControllerBase
       '#markup' => $this->getContent("kidiklik_recrute", $service->getTermDepartement(0))
     ];
   }
+  
+  /**
+   * Contact.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function contact()
+  {
+    $page_dep = \Drupal::service('kidiklik.service')->getPageDepartement();
+    $renderer = \Drupal::service("renderer");
+      $build = [
+        '#markup' => sprintf('<div class="jumbotron mt-4 pt-3 pb-3"><p><b class="bleu titre_h1">Coordonnées</b></p><b>Nom :</b> %s<br><b>Téléphone :</b> <a href="tel:%s">%s</a><br><b>E-mail :</b> <a href="mailto:%s">%s</a></div>',
+          $page_dep->get('field_societe')->value,
+          $page_dep->get('field_telephone')->value,
+          $page_dep->get('field_telephone')->value,
+          $page_dep->get('field_e_mail')->value,
+          $page_dep->get('field_e_mail')->value
+        ),
+      ];
+    return $build;
+  }
 
   /**
    * Content.
